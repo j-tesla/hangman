eb0517fa4761304556e15792d87bfc098916ebfb
MockingJay <58279279+j-tesla@users.noreply.github.com>
2020-07-20 12:44:42 +0530

    Updated hangman.py
    Added a hint to guess the word

--- hangman.py
+++ hangman.py
@@ -4,11 +4,12 @@ print('H A N G M A N')
 
 words = ['python', 'java', 'kotlin', 'javascript']
 word = random.choice(words)
+hint = word[:3] + '-'*(len(word) - 3)
 
 survived_msg = 'You survived!'
 hanged_msg = 'You are hanged!'
 
-guess = input('Guess the word: ')
+guess = input('Guess the word ' + hint + ': ')
 
 if guess == word:
     print(survived_msg)
