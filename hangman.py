9b6b542040895eee33e11d34fd951ff8e58683ce
MockingJay <58279279+j-tesla@users.noreply.github.com>
2020-07-20 13:17:47 +0530

    Updated hangman.py
    Made it a letter guessing game

--- hangman.py
+++ hangman.py
@@ -9,9 +9,31 @@ hint = word[:3] + '-'*(len(word) - 3)
 survived_msg = 'You survived!'
 hanged_msg = 'You are hanged!'
 
-guess = input('Guess the word ' + hint + ': ')
+guess = '-'*len(word)
 
-if guess == word:
-    print(survived_msg)
-else:
+tries = 8
+while tries:
+    print('\n' + guess)
+    letter = input('Input a letter: ')
+    if letter not in word:
+        print('No such letter in the word')
+        tries -= 1
+    elif letter in guess:
+        print('No improvements')
+        tries -= 1
+    else:
+        for i in range(len(word)):
+            if word[i] == letter:
+                guess = guess[:i] + letter + guess[i+1:]
+        if word == guess:
+            print(guess + '\n' + 'You guessed the word!')
+            print(survived_msg)
+            break
+if tries == 0:
     print(hanged_msg)
+
+
+# if guess == word:
+#     print(survived_msg)
+# else:
+#     print(hanged_msg)
