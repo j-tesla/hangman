705cea1d031c385e66708a63a16b134057b12da0
MockingJay <58279279+j-tesla@users.noreply.github.com>
2020-07-20 12:35:02 +0530

    Update hangman.py
    Made a list of words to choode from

--- hangman.py
+++ hangman.py
@@ -1,11 +1,16 @@
+import random
+
 print('H A N G M A N')
+
+words = ['python', 'java', 'kotlin', 'javascript']
+word = random.choice(words)
 
 survived_msg = 'You survived!'
 hanged_msg = 'You are hanged!'
 
 guess = input('Guess the word: ')
 
-if guess == 'python':
+if guess == word:
     print(survived_msg)
 else:
     print(hanged_msg)
